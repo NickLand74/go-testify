0b5d0490493b9705611c6aec83c79e9c973db0ac
Landaryev Nikita <landaryevnick@gmail.com>
2024-10-02 15:40:27 +0200

    second-commit

--- precode.go
+++ precode.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 var cafeList = map[string][]string{
@@ -43,33 +39,6 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 	}
 
 	answer := strings.Join(cafe[:count], ",")
-
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	// Общее количество кафе в городе "Москва" - 4
-	totalCount := 4
-	city := "moscow"
-	count := totalCount + 1 // Запрашиваем больше, чем доступно кафе
-
-	req, err := http.NewRequest("GET", "/?city="+city+"&count="+strconv.Itoa(count), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// Проверяем, что статус ответа - OK
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	// Извлекаем тело ответа
-	body := responseRecorder.Body.String()
-
-	// Проверяем, что тело ответа содержит все кафе, разделенные запятыми
-	expectedBody := strings.Join(cafeList[city], ",")
-	assert.Equal(t, expectedBody, body)
 }
