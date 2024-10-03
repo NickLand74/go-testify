1459a5582cf21ab613f374f174650f3a7535704b
Landaryev Nikita <landaryevnick@gmail.com>
2024-10-03 13:51:10 +0200

    forth-commit

--- main_test.go
+++ main_test.go
@@ -13,15 +13,14 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Errorf("handler returned неверный код состояния: got %v want %v", status, http.StatusOK)
-	}
+	// Проверяем код состояния
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "handler returned неверный код состояния")
 
 	expected := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("handler вернул неожидаемое тело: got %v want %v", responseRecorder.Body.String(), expected)
-	}
-	// Этот тест проверяет, правильно ли хендлер возвращает все доступные кафе, когда передано значение `count`, превышающее общее количество кафе
+	// Проверяем тело ответа
+	require.Equal(t, expected, responseRecorder.Body.String(), "handler вернул неожидаемое тело")
+
+	// Этот тест проверяет, правильно ли хендлер возвращает все доступные кафе, когда передано значение count, превышающее общее количество кафе
 }
 
 func TestMainHandlerWhenCityIsWrong(t *testing.T) {
@@ -31,15 +30,14 @@ func TestMainHandlerWhenCityIsWrong(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("handler вернул неверный код состояния: got %v want %v", status, http.StatusBadRequest)
-	}
+	// Проверяем код состояния
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code, "handler вернул неверный код состояния")
 
 	expected := "wrong city value"
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("handler вернул неожиданное тело: got %v want %v", responseRecorder.Body.String(), expected)
-	}
-	// Этот тест проверяет, отвечает ли хендлер кодом `400 Bad Request` и соответствующим сообщением об ошибке, если передан неправильный город
+	// Проверяем тело ответа
+	require.Equal(t, expected, responseRecorder.Body.String(), "handler вернул неожиданное тело")
+
+	// Этот тест проверяет, отвечает ли хендлер кодом 400 Bad Request и соответствующим сообщением об ошибке, если передан неправильный город
 }
 
 func TestMainHandlerWhenCountIsMissing(t *testing.T) {
@@ -49,14 +47,15 @@ func TestMainHandlerWhenCountIsMissing(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("handler вернул неверный код состояния: got %v want %v", status, http.StatusBadRequest)
-	}
+	// Проверяем код состояния
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code, "handler вернул неверный код состояния")
 
 	expected := "count missing"
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("handler вернул неожиданное тело: got %v want %v", responseRecorder.Body.String(), expected)
-	}
-	// Этот тест проверяет, отвечает ли хендлер кодом `400 Bad Request` и соответствующим сообщением об ошибке, когда параметр `count` отсутствует в запросе.
-	// действительно файл precode_test.go почему то не подгрузился в прошлый раз
+	// Проверяем тело ответа
+	require.Equal(t, expected, responseRecorder.Body.String(), "handler вернул неожиданное тело")
+
+	// Проверяем, что тело ответа не пустое
+	assert.NotEmpty(t, responseRecorder.Body.String(), "тело ответа должно быть не пустым")
+
+	// Этот тест проверяет, отвечает ли хендлер кодом 400 Bad Request и соответствующим сообщением об ошибке, когда параметр count отсутствует в запросе.
 }
